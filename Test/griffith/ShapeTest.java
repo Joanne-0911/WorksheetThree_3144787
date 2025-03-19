ceb270872191184a3a80149a2f383dd9e39d7564
Joanne Thomas <you@example.com>
2025-03-19 21:57:05 +0000

    Failed Unit Test 2

--- Test/griffith/ShapeTest.java
+++ Test/griffith/ShapeTest.java
@@ -20,6 +20,11 @@ public class ShapeTest {
 
     @Test
     public void testRhombus() {
+    	 Rhombus rhombus = new Rhombus("Rhombus", 6.0, 8.0, 5.0);
+         
+         assertEquals(24.0, rhombus.area(), 0); // EPSILON = 0
+         assertEquals(20.0, rhombus.perimeter(), 0);
+         assertEquals("Shape: Rhombus, Diagonal1: 6.0, Diagonal2: 8.0, Side: 5.0", rhombus.toString());
         
     }
 
